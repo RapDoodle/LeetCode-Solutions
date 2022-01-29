98cf4ea28547ba0f656effeae5d01a7db6695bed
RapDoodle <bowenbhwu@gmail.com>
2022-01-29 19:55:44 +0800

    Add problem 29

---
+++ cpp/0001 - 0099/29. Divide Two Integers/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+        
+        // `true` when the dividend and divisor has the same sign
+        bool sign = (dividend >= 0) == (divisor >= 0);
+        
+        long dvd = labs(dividend);
+        long dvs = labs(divisor);
+        
+        long quot = 0;
+        
+        while (dvd - dvs >= 0) {
+            int count = 0;
+            while (dvd - (dvs << count << 1) >= 0)
+                count++;
+            
+            quot += 1 << count;
+            dvd -= dvs << count;
+        }
+        
+        return sign ? quot : -quot;
+    }
+};
