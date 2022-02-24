5983ae44cc94af0a46ad74ed1f843109f3829a3e
Bowen <bowenbhwu@gmail.com>
2022-02-25 00:46:07 +0800

    Add problem 633

---
+++ cpp/0600-0699/633. Sum of Square Numbers/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        // When b = 0, we have a^2 = c. Thus, the maximum
+        // number allowed for a is a <= sqrt(c) 
+        int maxA = sqrt(c);
+        for (int a = 0; a <= maxA; ++a) {
+            // b can be estimated using 
+            // b = sqrt(c - a^2)
+            int b = sqrt(c - a*a);
+            
+            // When a valid pair is found
+            if (a*a + b*b == c)
+                return true;
+        }
+        return false;
+    }
+};
