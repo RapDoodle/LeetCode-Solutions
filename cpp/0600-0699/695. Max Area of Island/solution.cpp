a8303efd066caf75ca2f4f872ae9d3583207dbf5
Bowen <bowenbhwu@gmail.com>
2022-03-01 00:47:19 +0800

    Add problem 695

---
+++ cpp/0600-0699/695. Max Area of Island/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int dfs(vector<vector<int>>& grid, vector<vector<int>>& visited, int i, int j) {
+        // Base cases:
+        //  1. Out of bound
+        //  2. The grid is not an island
+        //  3. The grid has been visited
+        if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] == 0 || visited[i][j] == 1)
+            return 0;
+        
+        // Set the current grid as visited
+        visited[i][j] = 1;
+        
+        // Recursive case: look for islands in four directions
+        return 1 + dfs(grid, visited, i+1, j)
+                 + dfs(grid, visited, i-1, j) 
+                 + dfs(grid, visited, i, j+1) 
+                 + dfs(grid, visited, i, j-1);
+    }
+    
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int rows = grid.size(), cols = grid[0].size(), maxArea = 0;
+        vector<vector<int>> visited(rows, vector<int>(cols));
+        
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                int area = dfs(grid, visited, i, j);
+                if (area > maxArea)
+                    maxArea = area;
+            }
+        }
+        return maxArea;
+    }
+};
