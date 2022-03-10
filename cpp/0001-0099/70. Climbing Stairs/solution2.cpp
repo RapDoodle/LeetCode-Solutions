6a99f61e9412dc1e1b7e44173ef236385ca2bfc5
Bowen <bowenbhwu@gmail.com>
2022-03-10 16:14:13 +0800

    Update problem 70

--- cpp/0001-0099/70. Climbing Stairs/solution2.cpp
+++ cpp/0001-0099/70. Climbing Stairs/solution2.cpp
@@ -7,9 +7,9 @@ public:
         
         // Essentially solving:
         //  f(n) = f(n-1) + f(n-2) [Fibonacci sequence]
-        int prev1 = 1, prev2 = 1;
+        int prev1 = 1, prev2 = 1, temp;
         for (int i = 2; i <= n; ++i) {
-            int temp = prev1;
+            temp = prev1;
             prev1 = prev1 + prev2;
             prev2 = temp;
         }
