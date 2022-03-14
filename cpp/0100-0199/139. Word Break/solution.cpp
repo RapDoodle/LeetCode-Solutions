74bafcb12a34dfcbc10f8e36a2342bce819d0035
Bowen <bowenbhwu@gmail.com>
2022-03-14 12:28:16 +0800

    Add problem 139

---
+++ cpp/0100-0199/139. Word Break/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.length();
+        
+        // Dynamic programming transfer function:
+        //  dp[i] = dp[i-len], when s[i-len, i] is in wordDict
+        //        = false, when s[i-len, i] is not in wordDict
+        //        = true, when i=0 (the initial state)
+        vector<bool> dp(n+1, false);
+        dp[0] = true;
+        for (int i = 1; i <= n; ++i) {
+            for (const auto& word : wordDict) {
+                int len = word.length();
+                if (i >= len && s.substr(i-len, len) == word && dp[i-len])
+                    dp[i] = true;
+            }
+        }
+        
+        return dp[n];
+    }
+};
