9d51e79cf4f94fd9ed3bacdb32d8a1c00960ce8c
Bowen <bowenbhwu@gmail.com>
2022-03-23 16:07:17 +0800

    Update problem 121

--- cpp/0100-0199/121. Best Time to Buy and Sell Stock/solution.cpp
+++ cpp/0100-0199/121. Best Time to Buy and Sell Stock/solution.cpp
@@ -8,13 +8,12 @@ public:
         int l = 0, r = 1, maxProfit = 0;
         while (r < prices.size()) {
             int profit = prices[r] - prices[l];
-            if (profit > 0) {
+            if (profit > 0)
                 maxProfit = max(profit, maxProfit);
-            } else {
-                // We found a point with an even lower price
+            else
+                // Found a point with an even lower price
                 l = r;
-            }
-            r++;
+            ++r;
         }
         
         return maxProfit;
