d526be9f352f8341947d55ec398bfe94eb71e13c
Bowen <bowenbhwu@gmail.com>
2022-03-30 01:22:43 +0800

    Add problem 769

---
+++ cpp/0700-0799/769. Max Chunks To Make Sorted/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        
+        int chunks = 0, curMax = 0;
+        for (int i = 0; i < arr.size(); ++i) {
+            // When the current maximum is greater than the 
+            // index, it indicates that the current value must 
+            // be added to the current chunk, the chunk count 
+            // is unchanged.
+            // We add 1 to the chunk count when 
+            //  (1) The current maximum matches the index, meaning
+            //      the current position does not need to be sorted
+            //      (forms a chunk with itself). For example,
+            //          arr = [0,1,2,3,4], i = 1, 
+            //          [1] can form a chunk with itself.
+            //  (2) The interval between the last sorted position 
+            //      and the i-th position can be arranged into a
+            //      chunk (to be sorted). For example,
+            //          arr = [1,0,2,3,4], i = 1, 
+            //          [1,0] need to be sorted.
+            curMax = max(curMax, arr[i]);
+            if (curMax == i)
+                ++chunks;
+        }
+        return chunks;
+    }
+};
