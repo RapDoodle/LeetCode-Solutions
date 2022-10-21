9234208bb68b43ed12e0d77984e333cb9fbc482f
Bohui WU <bowenbhwu@gmail.com>
2022-10-21 09:46:11 +0800

    Update: Problem 5 readability and performance

--- cpp/0001-0099/05. Longest Palindromic Substring/solution1.cpp
+++ cpp/0001-0099/05. Longest Palindromic Substring/solution1.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     /* Dynamic programming */ 
-    string longestPalindrome(string s) {
+    string longestPalindrome(const string& s) {
         int m = s.length(), lo = 0, maxLen = 1;
 
         // Initialize an array of size m * m
         vector<vector<int>> dp(m, vector<int>(m));
 
-        // Fill all diagonal with 1
+        // Fill all diagonals with 1
         for (int i = 0; i < m; ++i)
             dp[i][i] = 1;
 
         // Check for substrings of length > 2
-        int valid = 0, j = 0;
-        for (int ofst = 1; ofst < m; ++ofst) {
-            for (int i = 0; i < m - ofst; ++i) {
-                j = i + ofst;
-                if (ofst == 1)
-                    valid = dp[i][j] = s[i] == s[j] ? 1 : 0;
+        for (int strOfst = 1; strOfst < m; ++strOfst) {
+            for (int i = 0; i < m - strOfst; ++i) {
+                // Calculate the upper bound of the substring
+                int j = i + strOfst;
+                // Check the two ends of the interval. 
+                // If the two ends equal (s[i] == s[j]), 
+                // and the substring in the middle 
+                // [i+1..j-1] is also a palindrome,
+                // [i..j] is also a palindrome
+                if (strOfst == 1)
+                    dp[i][j] = (s[i] == s[j] ? 1 : 0);
                 else
-                    valid = dp[i][j] = (s[i] == s[j] && dp[i+1][j-1] == 1) ? 1 : 0;
-                if (valid) {
-                    int length = ofst + 1;
+                    dp[i][j] = (s[i] == s[j] && dp[i+1][j-1] == 1) ? 1 : 0;
+                if (dp[i][j]) {
+                    int length = strOfst + 1;
                     if (length > maxLen) {
                         maxLen = length;
                         lo = i;
