38783c989e3079dd69eb6208ab2e9f4771d86778
Bohui WU <bohuibhwu@gmail.com>
2023-02-17 21:26:22 +0800

    Update: Problem 167

--- cpp/0100-0199/167. Two Sum II - Input Array Is Sorted/solution1.cpp
+++ cpp/0100-0199/167. Two Sum II - Input Array Is Sorted/solution1.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    /* Two-pointers */
+    /* Two pointers */
     vector<int> twoSum(vector<int>& numbers, int target) {
         int l = 0, r = numbers.size() - 1;
-        
         while (l < r) {
             int sum = numbers[l] + numbers[r];
-            if (sum == target) {
+            if (sum == target)
                 return {l+1, r+1};
-            }
-            if (sum > target) {
+            if (sum > target)
                 // sum > target, need to reduce the sum
                 --r;
-            } else {
+            else
                 // sum < target, need to increase the sum
                 ++l;
-            }
         }
         
         // No matching solution (beyond the constraints)
